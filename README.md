a25cebb89157496890c98e660b33f247de03a2c4
zzq0516 <980778982@qq.com>
2024-07-18 16:29:42 +0800

    InitVersion
    SimpleModel
    
    SimpleModel use join
    
    SimpleModel add Mute Lock
    
    add conditional var
    
    fix code style
    
    some problem in this version
    
    muilti cos and producer
    
    just work on debug mode no think why
    
    fix Problem
    
    simpleDemo1Totol
    
    simpleDemo2TTotal

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ThreadLearn
-ThreadLearn
+This projuct is used by ThreadLearn 
+## verson1 is muilt cosumer and muilt producers demo learen cv and lock
---
+++ cmake-build-debug/CMakeFiles/ThreadLearn.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ThreadLearn.dir/main.cpp.obj"
+  "CMakeFiles/ThreadLearn.dir/main.cpp.obj.d"
+  "ThreadLearn.exe"
+  "ThreadLearn.exe.manifest"
+  "ThreadLearn.pdb"
+  "libThreadLearn.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ThreadLearn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
