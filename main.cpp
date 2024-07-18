d0dae1287a338628385bb02cd7ab1039c2f37af5
zzq0516 <980778982@qq.com>
2024-07-18 16:40:29 +0800

    SimpleModel

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <iterator>
+#include <queue>
+
+std::queue<int> mQueue;
+
+void producers() {
+  mQueue.push(1);
+  mQueue.push(2);
+  mQueue.push(3);
+  mQueue.push(4);
+  mQueue.push(5);
+}
+
+void costormer() {
+  while (!mQueue.empty()) {
+    std::cout << "costormer get val:" << mQueue.front() << std::endl;
+    mQueue.pop();
+  }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  producers();
+  costormer();
 }
