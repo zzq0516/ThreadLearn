fafe337d912a6a8862c09770f81509b2771fc92c
zzq0516 <980778982@qq.com>
2024-07-18 17:02:00 +0800

    SimpleModel add Mute Lock

--- main.cpp
+++ main.cpp
@@ -1,30 +1,49 @@
 #include <iostream>
 #include <iterator>
+#include <mutex>
 #include <queue>
 #include <thread>
 
 std::queue<int> mQueue;
+std::mutex mlock;
 
 void producers() {
+  mlock.lock();
   mQueue.push(1);
+  mlock.unlock();
+  mlock.lock();
   mQueue.push(2);
+  mlock.unlock();
+  mlock.lock();
   mQueue.push(3);
+  mlock.unlock();
+  mlock.lock();
   mQueue.push(4);
+  mlock.unlock();
+  mlock.lock();
   mQueue.push(5);
+  mlock.unlock();
+  mlock.lock();
   mQueue.push(11);
+  mlock.unlock();
+  mlock.lock();
   mQueue.push(0);
+  mlock.unlock();
 }
 
 void costormer() {
   while (true) {
     while (!mQueue.empty()) {
+      mlock.lock();
       auto val = mQueue.front();
       if (val == 0) {
         mQueue.pop();
+        mlock.unlock();
         return;
       }
       std::cout << "costormer get val:" << val << std::endl;
       mQueue.pop();
+      mlock.unlock();
     }
   }
 }
