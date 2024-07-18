e4dcfb6b0352305e2946e2dc1603d3895f078cc1
zzq0516 <980778982@qq.com>
2024-07-18 16:49:25 +0800

    SimpleModel use join

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iterator>
 #include <queue>
+#include <thread>
 
 std::queue<int> mQueue;
 
@@ -10,16 +11,27 @@ void producers() {
   mQueue.push(3);
   mQueue.push(4);
   mQueue.push(5);
+  mQueue.push(11);
+  mQueue.push(0);
 }
 
 void costormer() {
-  while (!mQueue.empty()) {
-    std::cout << "costormer get val:" << mQueue.front() << std::endl;
-    mQueue.pop();
+  while (true) {
+    while (!mQueue.empty()) {
+      auto val = mQueue.front();
+      if (val == 0) {
+        mQueue.pop();
+        return;
+      }
+      std::cout << "costormer get val:" << val << std::endl;
+      mQueue.pop();
+    }
   }
 }
 
 int main() {
-  producers();
+  std::thread t1(producers);
+  // 这里阻塞执行 join 和单线程无区别
   costormer();
+  t1.join();
 }
